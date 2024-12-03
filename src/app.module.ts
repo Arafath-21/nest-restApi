c02618e0ab84b310ae9c830166d88de54d81f4a0
Arafath-21 <arafathyas21@gmail.com>
2024-12-03 18:29:13 +0530

    deploy v1.0

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,18 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
+      ignoreEnvFile: process.env.NODE_ENV === 'production', // Skip in production
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().default('localhost'),
+        POSTGRES_PORT: Joi.number().default(5432),
+      }),
     }),
     AuthModule,
     UserModule,
